2063569dae680ba3594a4022b0e8bf9724736f06
herbertfss <herbertsantows@gmail.com>
2023-06-29 22:46:38 -0300

    javascript

---
+++ logica/condicao/index.js
@@ -0,0 +1,41 @@
+//
+const pessoa = 'Herbert'
+let idade = 21
+
+if ( idade > 17) {
+    console.log('Maior que 17', idade)
+}
+
+else if( idade > 15){
+    console.log('Maior que 15', idade)
+}
+
+else{
+    console.log('Menor que 16', idade)
+}
+
+let num1 = 10
+let num2 = 20
+
+const resultado = num1 / num2
+
+if ( resultado === 0) {
+    console.log()
+}
+
+
+let num = 0
+
+if(num > 0){
+    console.log( num, 'positivo')
+}
+
+else if(num < 0){
+    console.log(num, 'negativo')
+}
+
+else {
+    console.log(num, 'zero')
+}
+
+
